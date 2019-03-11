6ff762346b5f9b7c9af45c489bc67be1aee08ef5
守夜人 <39370150+2292527883@users.noreply.github.com>
2019-03-11 19:03:21 +0800

    Add files via upload

---
+++ 习题14.py
@@ -0,0 +1,22 @@
+from sys import argv
+script,user_name=argv
+prompt = '>'
+
+print(f"Hi {user_name} , I'm the {script} script  ")
+print("I'd like to ask you a few question")
+print(f"Do you laike me {user_name} ?")
+likes =input(prompt)
+
+print(f"where do you live {user_name} ?")
+lives = input(prompt)
+
+print("what kind of computer do you have ?")
+computer = input(prompt)
+print(f"""
+Alright , so you said {likes} About Liking me.
+You live in {lives}. Not sure where that is .
+Anad you have a {computer} computer . Nice.
+""")
+
+sdsd value=Warning value=
+wd a ow file=a value=a dwjiji wj value=
